cd01f2ee3e06d753432ef54f0da952871f2f3a67
martonistvan0330 <mistvan0330@gmail.com>
2023-10-29 14:24:14 +0100

    Fix - Register form - Error message rendering

--- HomeworkManager.Angular/ClientApp/src/app/auth-module/register/register.component.ts
+++ HomeworkManager.Angular/ClientApp/src/app/auth-module/register/register.component.ts
@@ -46,66 +46,11 @@ export class RegisterComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.registerForm = new FormGroup({
-      email: new FormControl('', {
-        validators: [Validators.required, Validators.email],
-        asyncValidators: [this.uniqueEmailAsyncValidator.validate.bind(this.uniqueEmailAsyncValidator)],
-        updateOn: 'blur'
-      }),
-      username: new FormControl('', {
-        validators: [Validators.required],
-        asyncValidators: [this.uniqueUsernameAsyncValidator.validate.bind(this.uniqueUsernameAsyncValidator)],
-        updateOn: 'blur'
-      }),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6),
-        containsLowerCaseValidator(1),
-        containsUpperCaseValidator(1),
-        containsDigitValidator(1)
-      ])
-    });
+    this.formSetup();
 
-    this.email.valueChanges
-      .subscribe(() => {
-        if (this.email.valid) {
-          this.emailError = Errors.NoError;
-        } else if (this.email.errors?.['required']) {
-          this.emailError = Errors.Required;
-        } else if (this.email.errors?.['email']) {
-          this.emailError = Errors.Email;
-        } else if (this.email.errors?.['uniqueEmail']) {
-          this.emailError = Errors.Unique;
-        }
-      });
-
-    this.username.valueChanges
-      .subscribe(() => {
-        if (this.username.valid) {
-          this.usernameError = Errors.NoError;
-        } else if (this.username.errors?.['required']) {
-          this.usernameError = Errors.Required;
-        } else if (this.username.errors?.['uniqueUsername']) {
-          this.usernameError = Errors.Unique;
-        }
-      });
-
-    this.password.valueChanges
-      .subscribe(() => {
-        if (this.password.valid) {
-          this.passwordError = Errors.NoError;
-        } else if (this.password.errors?.['required']) {
-          this.passwordError = Errors.Required;
-        } else if (this.password.errors?.['minlength']) {
-          this.passwordError = Errors.MinLength;
-        } else if (this.password.errors?.['containsLowerCase']) {
-          this.passwordError = Errors.ContainsLowerCase;
-        } else if (this.password.errors?.['containsUpperCase']) {
-          this.passwordError = Errors.ContainsUpperCase;
-        } else if (this.password.errors?.['containsDigit']) {
-          this.passwordError = Errors.ContainsDigit;
-        }
-      });
+    this.emailControlSetup();
+    this.usernameControlSetup();
+    this.passwordControlSetup();
   }
 
   register() {
@@ -133,6 +78,87 @@ export class RegisterComponent implements OnInit {
         error: error => {
           this.snackBarService.error('Registration failed', error.error);
           this.isLoading = false;
+        }
+      });
+  }
+
+  private formSetup() {
+    this.registerForm = new FormGroup({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+        asyncValidators: [this.uniqueEmailAsyncValidator.validate.bind(this.uniqueEmailAsyncValidator)],
+        updateOn: 'change'
+      }),
+      username: new FormControl('', {
+        validators: [Validators.required],
+        asyncValidators: [this.uniqueUsernameAsyncValidator.validate.bind(this.uniqueUsernameAsyncValidator)],
+        updateOn: 'blur'
+      }),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        containsLowerCaseValidator(1),
+        containsUpperCaseValidator(1),
+        containsDigitValidator(1)
+      ])
+    });
+  }
+
+  private emailControlSetup() {
+    this.email.statusChanges
+      .subscribe(() => {
+        if (this.email.status === 'VALID') {
+          this.emailError = Errors.NoError;
+        }
+
+        if (this.email.status === 'INVALID') {
+          if (this.email.errors?.['required']) {
+            this.emailError = Errors.Required;
+          } else if (this.email.errors?.['email']) {
+            this.emailError = Errors.Email;
+          } else if (this.email.errors?.['uniqueEmail']) {
+            this.emailError = Errors.Unique;
+          }
+        }
+      });
+  }
+
+  private usernameControlSetup() {
+    this.username.statusChanges
+      .subscribe(() => {
+        if (this.username.status === 'VALID') {
+          this.usernameError = Errors.NoError;
+        }
+
+        if (this.username.status === 'INVALID') {
+          if (this.username.errors?.['required']) {
+            this.usernameError = Errors.Required;
+          } else if (this.username.errors?.['uniqueUsername']) {
+            this.usernameError = Errors.Unique;
+          }
+        }
+      });
+  }
+
+  private passwordControlSetup() {
+    this.password.statusChanges
+      .subscribe(() => {
+        if (this.password.status === 'VALID') {
+          this.passwordError = Errors.NoError;
+        }
+
+        if (this.password.status === 'INVALID') {
+          if (this.password.errors?.['required']) {
+            this.passwordError = Errors.Required;
+          } else if (this.password.errors?.['minlength']) {
+            this.passwordError = Errors.MinLength;
+          } else if (this.password.errors?.['containsLowerCase']) {
+            this.passwordError = Errors.ContainsLowerCase;
+          } else if (this.password.errors?.['containsUpperCase']) {
+            this.passwordError = Errors.ContainsUpperCase;
+          } else if (this.password.errors?.['containsDigit']) {
+            this.passwordError = Errors.ContainsDigit;
+          }
         }
       });
   }
